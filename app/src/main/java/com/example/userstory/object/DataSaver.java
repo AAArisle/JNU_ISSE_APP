2a7e0f28110aca23d9b27cbd5b71c9a5831b4e1c
AAArisle <2416948889@qq.com>
2024-06-07 17:30:06 +0800

    保存json文件的实现

--- app/src/main/java/com/example/userstory/object/DataSaver.java
+++ app/src/main/java/com/example/userstory/object/DataSaver.java
@@ -1,8 +1,23 @@
 package com.example.userstory.object;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DataSaver {
     public void save(Object data, String filename) {
-
+        try {
+            File file = new File(filename);
+            FileWriter writer = new FileWriter(file);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(data, writer);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Object load(Class<?> classType, String filename) {
